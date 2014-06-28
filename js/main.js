7420ec9cc3e3fa1e45c860a4091946be2c4cc225
Michael Peterson <mpeterson418@gmail.com>
2014-06-28 01:05:50 -0400

    Made a dartlang.org style footer.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Not sure why the body's height is
+  // sometimes a weird size when it loads.
+  window.setTimeout(onResize, 100);
+  $(window).resize(onResize);
+})
+
+function onResize() {
+  var bHeight = $("body").height();
+  var lHeight = $("#links").height() + 50;
+  $("html").css("height", "calc(" + bHeight + "px + " + lHeight + "px)");
+}
